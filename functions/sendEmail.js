eea4397b9c43d904d09e2e070b8c4781cf2eba4b
gorkemtand <tandogangorkem@gmail.com>
2023-06-25 21:00:25 +0300

    subject

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -13,13 +13,11 @@ exports.handler = async (event, context) => {
       to: process.env.CONTACT_EMAIL, // Replace with your email address
       from: email,
       subject: 'New Contact Form Submission',
-      text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${message}`,
+      text: `Name: ${name}\nSubject: ${subject}\nEmail: ${email}\n\nMessage: ${message}`,
     };
 
     // Send the email
     await sgMail.send(msg);
-    console.log(process.env.SENDGRID_API_KEY);
-    console.log(process.env.CONTACT_EMAIL);
     return {
       statusCode: 200,
       body: JSON.stringify({message: 'Email sent successfully'}),
